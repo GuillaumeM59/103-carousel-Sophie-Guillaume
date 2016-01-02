ce5a5970b133a767594cd74abdd4d7a0e98561de
GuillaumeM59 <manier-guillaume@orange.fr>
2016-01-02 23:13:55 +0100

    add automatic break animation with play break buttons - complete head of html

--- js/caroussel.js
+++ js/caroussel.js
@@ -1,17 +1,40 @@
+
 // add slideshow conteneur and a node of images inside
 var contSlideShow = document.getElementById("slideshow");
 var imgs = contSlideShow.getElementsByTagName("img");
 var curImage = 0; //current image begin at 0 but will change
+var timer;
 
 // create icons to move left right
 var elRightArrow = document.createElement("i");
-elRightArrow.className = " arrowright fa fa-chevron-right fa-3x ";
+elRightArrow.className = "navBTN arrowright fa fa-chevron-right fa-4x";
 var elLeftArrow = document.createElement("i");
-elLeftArrow.className = " arrowleft fa fa-chevron-left fa-3x ";
+elLeftArrow.className = "navBTN arrowleft fa fa-chevron-left fa-4x ";
+var elPauseBtn = document.createElement("i");
+elPauseBtn.className = "navBTN playPause fa fa-pause-circle fa-3x ";
+var elPlayBtn = document.createElement("i");
+elPlayBtn.className = "navBTN playPause fa fa-play-circle fa-3x ";
+
 
 // add icons on html document in the slideshow
 contSlideShow.appendChild(elRightArrow);
 contSlideShow.appendChild(elLeftArrow);
+contSlideShow.appendChild(elPlayBtn);
+contSlideShow.appendChild(elPauseBtn);
+
+
+// changing img each 4s
+var animate= function() {
+  elPlayBtn.style.display = "none";
+  elPauseBtn.style.display = "block";
+ timer = window.setInterval(nextSlide, 4000);
+};
+//stop animate
+var stopanim= function() {
+  elPlayBtn.style.display = "block";
+  elPauseBtn.style.display = "none";
+  window.clearInterval(timer);
+};
 
 //function who going to next img
 var nextSlide = function() {
@@ -37,6 +60,12 @@ var onClickmovedw = function() {
   if (curImage < 0) {
     curImage = imgs.length - 1;
   }
+  stopanim();
+};
+// function who going to the previous img
+var onClickmovenext = function() {
+  nextSlide();
+  stopanim();
 };
 
 // function who initialize slideshow
@@ -50,9 +79,9 @@ var slideShow = function() {
 
 //call the slideshow
 slideShow();
-// changing img each 4s
-var timer = window.setInterval(nextSlide, 4000);
-
+animate();
 // giving actions to icons
 elLeftArrow.addEventListener("click", onClickmovedw, false);
-elRightArrow.addEventListener("click", nextSlide, false);
+elRightArrow.addEventListener("click", onClickmovenext, false);
+elPlayBtn.addEventListener("click", animate, false);
+elPauseBtn.addEventListener("click", stopanim, false);
